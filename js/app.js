d25861c79398677d4458b1c911f0e8a5098187a7
Sergio Milici <sergio.milici@gmail.com>
2015-08-18 21:33:16 +0000

    Creado archivo npm junto con todas sus dependencias. Eliminado el uso de $scope en controllers por controllerAs

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
 'use strict';
 
 /* Controllers */
-var app = angular.module('agendaApp', [])
+var app = angular.module('agendaApp', []);
 
-app.controller('AgendaController', function ($scope) {
+app.controller('AgendaController', function () {
 	/* scope */
-	$scope.contactos = [
+	this.contactos = [
 	    {
-	    	"nombre" : "Juan", 
-	    	"apellido" : "Perez", 
-	    	"telefono" : "321472352", 
-	    	"email" : "juan@gmail.com", 
+	    	"nombre" : "Juan",
+	    	"apellido" : "Perez",
+	    	"telefono" : "321472352",
+	    	"email" : "juan@gmail.com",
 	    	"direccion" : "Corrientes 1234, 3B"
 	    },
 	    { "nombre" : "Pablo", "apellido" : "Alvarez", "telefono" : "666", "email" : "juan@gmail.com", "direccion" : "Corrientes 1234, 3B"},
 	    { "nombre" : "Maria", "apellido" : "Rodriguez", "telefono" : "47584777", "email" : "juan@gmail.com", "direccion" : "Corrientes 1234, 3B"},
 	    { "nombre" : "Andrea", "apellido" : "Garcia", "telefono" : "888890", "email" : "juan@gmail.com", "direccion" : "Corrientes 1234, 3B"}
 	];
-	
-	$scope.textoBusqueda = '';
-	
-	$scope.filtroActual = '';
-	
-	$scope.buscar = function() {
-		$scope.filtroActual = $scope.textoBusqueda
-	}
+
+	this.textoBusqueda = '';
+
+	this.filtroActual = '';
+
+	this.buscar = function() {
+		this.filtroActual = this.textoBusqueda
+	};
+
 });
   
   
